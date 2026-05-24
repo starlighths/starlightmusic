d08905eb0fca7803f4af66042ae8c98aa4bf10e8
starlighths <271805564+starlighths@users.noreply.github.com>
2026-05-24 18:34:00 +0800

    upd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.getElementById('app-container');
+
 for (let i = 0; i < 4; i++) {
     const square = document.createElement('div');
     square.classList.add('music-square');
